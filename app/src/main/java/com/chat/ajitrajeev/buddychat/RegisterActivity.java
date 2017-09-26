4508966ddad099a5a0e7649a643870a1f763b52f
ajit-999 <ajitkumarjain123@gmail.com>
2017-09-26 23:44:28 +0530

    Added Login Porcess

--- app/src/main/java/com/chat/ajitrajeev/buddychat/RegisterActivity.java
+++ app/src/main/java/com/chat/ajitrajeev/buddychat/RegisterActivity.java
@@ -1,11 +1,13 @@
 package com.chat.ajitrajeev.buddychat;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -15,11 +17,14 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.w3c.dom.Text;
+
 public class RegisterActivity extends AppCompatActivity {
     private TextInputLayout mDisplayName,mEmail,mPassword;
     private Button mCreateButton;
     private FirebaseAuth mAuth;
     private Toolbar mToolbar;
+    private ProgressDialog mRegProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +40,7 @@ public class RegisterActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Createn Account");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mAuth = FirebaseAuth.getInstance();
+        mRegProgress = new ProgressDialog(this);
 
         mCreateButton.setOnClickListener(new View.OnClickListener() { //create Account button action
             @Override
@@ -42,6 +48,13 @@ public class RegisterActivity extends AppCompatActivity {
                 String displayName = mDisplayName.getEditText().toString();
                 String email = mEmail.getEditText().toString();
                 String password = mPassword.getEditText().toString();
+                if (TextUtils.isEmpty(displayName)||TextUtils.isEmpty(email)|| TextUtils.isEmpty(password)){
+                    mRegProgress.setTitle("Registering User");
+                    mRegProgress.setMessage("Please wailt while we creating account ! ");
+                    mRegProgress.setCanceledOnTouchOutside(false);
+                    mRegProgress.show();
+                    register_user(displayName,email,password);
+                }
             }
         });
     }
@@ -50,12 +63,14 @@ public class RegisterActivity extends AppCompatActivity {
               @Override
               public void onComplete(@NonNull Task<AuthResult> task) {
                   if (task.isSuccessful()){
+                      mRegProgress.dismiss();
                       Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
                       startActivity(intent);
                       finish();
                   }
                   else {
-                      Toast.makeText(getApplicationContext(),"Your have some problem",Toast.LENGTH_LONG).show();
+                      mRegProgress.hide();
+                      Toast.makeText(getApplicationContext(),"Can not Sign in chek the form and try again ",Toast.LENGTH_LONG).show();
                   }
               }
           });
