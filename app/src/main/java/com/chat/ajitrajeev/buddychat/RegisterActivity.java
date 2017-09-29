e6a6ff2c4d720e241027f4cb09b4d5113f2c6df1
ajit-999 <ajitkumarjain123@gmail.com>
2017-09-29 17:06:27 +0530

    Added TabView and View pager

--- app/src/main/java/com/chat/ajitrajeev/buddychat/RegisterActivity.java
+++ app/src/main/java/com/chat/ajitrajeev/buddychat/RegisterActivity.java
@@ -64,6 +64,7 @@ public class RegisterActivity extends AppCompatActivity {
                   if (task.isSuccessful()){
                       mRegProgress.dismiss();
                       Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+                      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                       startActivity(intent);
                       finish();
                   }
