b1c7a31257268ce3ee83d2039733fbf858c57cec
rajeeviiit <singhrajiv774@gmail.com>
2017-10-08 12:45:27 +0530

    change

--- app/src/main/java/com/chat/ajitrajeev/buddychat/MainActivity.java
+++ app/src/main/java/com/chat/ajitrajeev/buddychat/MainActivity.java
@@ -14,6 +14,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
     private FirebaseAuth mAuth;
     private Toolbar mToolbar;
     private ViewPager mViewPager;
