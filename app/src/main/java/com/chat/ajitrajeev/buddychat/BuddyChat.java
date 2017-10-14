459a5a2f7ff87c6265685267153cfb21fcc0d0e0
ajit-999 <ajitkumarjain123@gmail.com>
2017-10-14 22:46:29 +0530

    Updating message page

--- app/src/main/java/com/chat/ajitrajeev/buddychat/BuddyChat.java
+++ app/src/main/java/com/chat/ajitrajeev/buddychat/BuddyChat.java
@@ -2,7 +2,13 @@ package com.chat.ajitrajeev.buddychat;
 
 import android.app.Application;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.OkHttpDownloader;
 import com.squareup.picasso.Picasso;
 
@@ -10,17 +16,54 @@ import com.squareup.picasso.Picasso;
  * Created by ajit on 2/10/17.
  */
 
-public class BuddyChat extends Application {
+public class BuddyChat extends Application{
+
+    private DatabaseReference mUserDatabase;
+    private FirebaseAuth mAuth;
+
     @Override
     public void onCreate() {
         super.onCreate();
+
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 
+        /* Picasso */
+
         Picasso.Builder builder = new Picasso.Builder(this);
-        builder.downloader(new OkHttpDownloader(this,Integer.MAX_VALUE));
-        Picasso built =builder.build();
+        builder.downloader(new OkHttpDownloader(this, Integer.MAX_VALUE));
+        Picasso built = builder.build();
         built.setIndicatorsEnabled(true);
         built.setLoggingEnabled(true);
         Picasso.setSingletonInstance(built);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser() != null) {
+
+            mUserDatabase = FirebaseDatabase.getInstance()
+                    .getReference().child("Users").child(mAuth.getCurrentUser().getUid());
+
+            mUserDatabase.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                    if (dataSnapshot != null) {
+
+                        mUserDatabase.child("online").onDisconnect().setValue(ServerValue.TIMESTAMP);
+
+                    }
+
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+        }
+
+
     }
+
+
 }
