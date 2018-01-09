feff09ef911e2f56360ec2180eab42ea207617b8
Ajit <ajitkumarjain123@gmail.com>
2018-01-09 07:45:22 +0530

    Added A Package Testing

---
+++ app/src/main/java/com/chat/ajitrajeev/buddychat/Testing/Kranti.java
@@ -0,0 +1,20 @@
+package com.chat.ajitrajeev.buddychat.Testing;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+/**
+ * Created by Aditya on 1/9/2018.
+ */
+
+public class Kranti extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("Branch Testing","Hello");
+    }
+}
